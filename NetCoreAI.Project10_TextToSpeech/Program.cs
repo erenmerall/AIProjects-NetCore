6ec630a774051d844af6f19cf90a4fecb0e0bb9f
Eren Meral <erenmeral02@gmail.com>
2025-10-22 19:14:05 +0300

    Text To Speech App

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        synthesizer.Volume = 100;  // Set volume to maximum 
+        synthesizer.Rate = 0;      // Set speaking rate to normal (konuşma hızı)
+
+        Console.Write("Enter text to convert to speech:");
+        string input = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(input))
+        {
+            synthesizer.Speak(input);
+        }
+
+        Console.ReadLine();
+    }
+}
